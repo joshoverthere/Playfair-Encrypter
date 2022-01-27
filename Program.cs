44c47020477370a81ce8b5b56a96fb976eae88d3
joshoverthere <contact@xeta.space>
2022-01-27 22:44:48 +0000

    Updates to key grid method.

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace Playfair
             //the letter x is excluded so the alphabet is 25 letters long and can fit in the grid
             List<string> alphabet = new List<string> { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "y" };
             string[,] grid = new string[5,5];
-            new List<string> lettersUsed = new List<string>();
+            List<string> lettersUsed = new List<string>();
 
             //format message and key (make lowercase, remove spaces)
             message = message.ToLower();
@@ -19,7 +19,7 @@ namespace Playfair
             key = key.Replace(" ", "");
 
 
-            string nextLetter;
+            string nextLetter = "";
 
             //loop through rows and columns of the grid
             for (int i = 0; i < 5; i++)
@@ -29,11 +29,39 @@ namespace Playfair
                     if (message.Length > 1)
                     {
                         //need to find a letter that hasn't already been used
-                        bool foundLetter
+                        bool foundLetter = false;
 
+                        while (!foundLetter)
+                        {
+                            Console.WriteLine("searching for a letter");
+                            //assume a letter has been found unless proven otherwise
+                            foundLetter = true;
 
-                        nextLetter = message.Substring(0, 1);
-                        message = message.Remove(0, 1);
+                            //make the next letter be the first letter of the remaining message
+                            if (message.Length > -1)
+                            {
+                                Console.WriteLine("wow");
+                                nextLetter = message.Substring(0, 1);
+                                message = message.Remove(0, 1);
+                            }
+                            else
+                            {
+                                nextLetter = alphabet[0];
+                                alphabet.RemoveAt(0);
+                            }
+
+                            //check if the nextletter matches a letter that has already been used...
+                            foreach (string letter in lettersUsed)
+                            {
+                                if (letter == nextLetter)
+                                {
+                                    //...and if it does, we didn't actually find a usable next letter!
+                                    foundLetter = false;
+                                }
+                            }
+                        }
+
+                        lettersUsed.Add(nextLetter);
                         Console.WriteLine(nextLetter);
                     }
                     else
