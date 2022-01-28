5ba062b36e6d22a429eeaafa65643f5ab24373ff
joshoverthere <contact@xeta.space>
2022-01-28 10:18:30 +0000

    Key grid method functioning.

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace Playfair
         static string[,] createGrid(string message, string key)
         {
             //the letter x is excluded so the alphabet is 25 letters long and can fit in the grid
-            List<string> alphabet = new List<string> { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "y" };
+            List<string> alphabet = new List<string> { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "y", "z" };
             string[,] grid = new string[5,5];
             List<string> lettersUsed = new List<string>();
 
@@ -18,64 +18,62 @@ namespace Playfair
             key = key.ToLower();
             key = key.Replace(" ", "");
 
-
-            string nextLetter = "";
-
             //loop through rows and columns of the grid
             for (int i = 0; i < 5; i++)
             {
                 for (int a = 0; a < 5; a++)
                 {
-                    if (message.Length > 1)
+                    bool letterFound = false;
+                    string nextLetter = "";
+
+                    //continue until a valid letter is found
+                    while (!letterFound)
                     {
-                        //need to find a letter that hasn't already been used
-                        bool foundLetter = false;
+                        //assume a valid letter has been found unless proven otherwise
+                        letterFound = true;
 
-                        while (!foundLetter)
+                        //if there are still parts of the message that haven't been added to the grid yet
+                        if (message.Length > 0)
                         {
-                            Console.WriteLine("searching for a letter");
-                            //assume a letter has been found unless proven otherwise
-                            foundLetter = true;
+                            //take the first letter of the message (and remove it for future cycles)
+                            nextLetter = message.Substring(0, 1);
+                            message = message.Remove(0, 1);
 
-                            //make the next letter be the first letter of the remaining message
-                            if (message.Length > -1)
+                            //if that letter is already in the grid then the letter is invalid, if not then add that letter to the list of used letters
+                            if (lettersUsed.Contains(nextLetter))
                             {
-                                Console.WriteLine("wow");
-                                nextLetter = message.Substring(0, 1);
-                                message = message.Remove(0, 1);
+                                letterFound = false;
                             }
                             else
                             {
-                                nextLetter = alphabet[0];
-                                alphabet.RemoveAt(0);
-                            }
-
-                            //check if the nextletter matches a letter that has already been used...
-                            foreach (string letter in lettersUsed)
-                            {
-                                if (letter == nextLetter)
-                                {
-                                    //...and if it does, we didn't actually find a usable next letter!
-                                    foundLetter = false;
-                                }
+                                lettersUsed.Add(nextLetter);
                             }
                         }
+                        //if there is no message left, use the rest of the alphabet
+                        else
+                        {
+                            //take the first letter of what remains of the alphabet (and remove it for future cycles)
+                            nextLetter = alphabet[0];
+                            alphabet.RemoveAt(0);
 
-                        lettersUsed.Add(nextLetter);
-                        Console.WriteLine(nextLetter);
+                            //if that letter has already been used in the grid then it's invalid, otherwise add it to list of letters used
+                            if (lettersUsed.Contains(nextLetter))
+                            {
+                                letterFound = false;
+                            }
+                            else
+                            {
+                                lettersUsed.Add(nextLetter);
+                            }
+                        }
                     }
-                    else
-                    {
-                        
-                        
-                    }
+
+                    //assign letter to grid
+                    grid[i,a] = nextLetter;
+                    Console.WriteLine(nextLetter);
                     
                 }
             }
-
-            message = message.Remove(0,1);
-            Console.WriteLine(message);
-
             return grid;
         }
         static void Main(string[] args)
